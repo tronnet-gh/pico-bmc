8cd68286df1c5a5e963c46f85e9421e1ebadf140
Arthur Lu <root@tronnet.net>
2024-02-28 22:56:22 -0800

    implement http using tcp sockets, remove cgi ssi lwip_http, remove makefsdata.py

--- Makefile
+++ Makefile
@@ -1,13 +1,9 @@
 .PHONY: all clean build makefsdata
 
-all: clean makefsdata build
+all: clean build
 
 clean:
 	$(MAKE) -C build/ clean
-	rm -rf wwwdata.c
-
-makefsdata:
-	python3 makefsdata.py
 
 build:
 	$(MAKE) -C build/
--- handlers.h
+++ handlers.h
@@ -8,8 +8,15 @@
 #define PW_STATE_UPDATE_REPEAT_DELAY_MS 100
 #define PW_STATE_INV 1
 
-bool current_state = false;
-struct repeating_timer * state_update_timer = NULL; 
+typedef struct CURRENT_STATE_T_ {
+	float voltage;
+	float tempC;
+	bool power_state;
+} CURRENT_STATE_T;
+
+struct repeating_timer * state_update_timer = NULL;
+
+CURRENT_STATE_T current_state;
 
 // handler fn to set the power switch pin to an active state
 int64_t pw_sw_on_async (alarm_id_t id, void * user_data) {
@@ -27,13 +34,15 @@ int64_t pw_sw_off_async (alarm_id_t id, void * user_data) {
 
 // hander fn to read from the power state
 bool update_current_state_async (repeating_timer_t * rt) {
-	current_state = gpio_get(PW_STATE_PIN) ^ PW_STATE_INV;
+	current_state.voltage = adc_read() * 3.3f / (1 << 12);
+    current_state.tempC = 27.0f - (current_state.voltage - 0.706f) / 0.001721f;
+	current_state.power_state = gpio_get(PW_STATE_PIN) ^ PW_STATE_INV;
 	return true; // continue repeating alarm
 }
 
 // handler fn called to attempt to set the power state to the requested state
-void bmc_power_handler (bool requested_state) {
-	if (requested_state != current_state) {
+void bmc_power_handler (bool requested_power_state) {
+	if (requested_power_state != current_state.power_state) {
 		add_alarm_in_ms(0, pw_sw_on_async, NULL, true);
 		add_alarm_in_ms(PW_SWITCH_DELAY_MS, pw_sw_off_async, NULL, true);
 	}
@@ -48,9 +57,16 @@ void bmc_handler_init () {
 	gpio_init(PW_STATE_PIN);
 	gpio_set_dir(PW_STATE_PIN, GPIO_IN);
 
+	// init adc input
+	adc_init();
+	adc_set_temp_sensor_enabled(true);
+	adc_select_input(4);
+
 	// set repeating timer for power state update
 	state_update_timer = malloc(sizeof(struct repeating_timer));
 	add_repeating_timer_ms(PW_STATE_UPDATE_REPEAT_DELAY_MS, update_current_state_async, NULL, state_update_timer);
+
+	DEBUG_printf("[BMC ] [OK ] BMC initialized successfully\n");
 }
 
 void bmc_handler_deinit () {
