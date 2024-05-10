ddb21ca4934edb6b815e1e6166f003cab721fd0e
ltcptgeneral <learthurgo@gmail.com>
2024-05-10 15:01:22 -0700

    add serial handler, move network init to network.h

--- template.secret.h
+++ template.secret.h
@@ -1,8 +1,8 @@
 #ifndef SECRET_H
 #define SECRET_H
 
-const char BMC_HOSTNAME[] = "pico_bmc";
-const char WIFI_SSID[] = "ssid";
-const char WIFI_PASS[] = "pass";
+const char BMC_HOSTNAME [] = "pico_bmc";
+const char WIFI_SSID [] = "ssid";
+const char WIFI_PASS [] = "pass";
 
 #endif
